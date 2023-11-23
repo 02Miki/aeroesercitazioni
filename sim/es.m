a8aa4c98f42955225a583242f286bb1133e17601
Michele <13dona.michele@gmail.com>
2023-11-23 18:13:15 +0100

    up to date sim

--- sim/es.m
+++ sim/es.m
@@ -1,4 +1,4 @@
-%%  1
+%%  2
 clc
 clear 
 close all
@@ -10,16 +10,14 @@ portata = 1000;
 beta = 60;
 betaRadianti = deg2rad(beta);
 
-
 velocita = portata/(rho*pi*(diametro/2)^2)
 
 vOrizz = velocita;
 u = velocita;
 
 vDeviata = u/cos(betaRadianti);
-
+ 
 deltaP = 1/2*rho*(0^2-(vDeviata*sin(betaRadianti))^2)
-
 
 
 Fx = (deltaP)*pi*(diametro/2)^2
@@ -27,7 +25,7 @@ Fy = rho*pi*(diametro/2)^2*u*(vDeviata*sin(betaRadianti))
 
 Ftot = sqrt(Fx^2+Fy^2);
 
-angolo = acos(Fx/Ftot)
+angoloRadianti = acos(Fx/Ftot)
 
 
 
