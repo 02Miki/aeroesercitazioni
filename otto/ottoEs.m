7323f27c06bb912107f306df0bdf348bfd398ce4
Miki <35926600+02Miki@users.noreply.github.com>
2023-12-04 18:45:17 +0100

    up to date

--- otto/ottoEs.m
+++ otto/ottoEs.m
@@ -6,18 +6,49 @@ close all
 
 diametro = 0.0135;
 R = diametro/2;
-lunghezza = 30;
+L = 30;
 deltaP = 1500;
-viscositaCinematica = 1.3*10^-6;
+viscositaCinematica = 1.3*10^-6; % ni
 densitaAcqua = 1000;
-viscositaDinamica = viscositaCinematica*densitaAcqua;
+viscositaDinamica = viscositaCinematica*densitaAcqua; % mu
 
 
-u = @(r) R^2/(4*viscositaDinamica)*deltaP/lunghezza*(1-r.^2./R^2);
+u = @(r) R^2/(4*viscositaDinamica)*deltaP/L*(1-r.^2./R^2).*r;
 
-portata = integral(@(r) R^2/(4*viscositaDinamica)*deltaP/lunghezza*(1-r.^2./R^2)*2*pi, 0, R)
+portata = integral(@(r) u(r).*2*pi, 0, R) % m^3/sec
 
-portataFatta = -pi/8*R^4/viscositaDinamica*deltaP/lunghezza % m3/s
+LITRI_TO_M3 = 1000;
+SECONDI_TO_MINUTI = 60;
+
+portataCorretta = portata*LITRI_TO_M3*SECONDI_TO_MINUTI
+
+vMedia = portata/(pi*R^2)
+Re = vMedia*diametro/viscositaCinematica
+
+% teta = linspace(0,2*pi);
+% lunghezza = linspace(0, L);
+% raggio = meshgrid(linspace(0, R), linspace(0, R), linspace(0, R));
+% [TETA, LUNGHEZZA] = meshgrid(teta, lunghezza);
+% [X, Y, Z] = pol2cart(TETA, R, LUNGHEZZA);
+% % [X,Y,Z] = meshgrid(x,y,z);
+% [x,y,z] = pol2cart(teta, R, lunghezza);
+% [XT, YT, ZT] = meshgrid(x,y,z);
+% 
+% hold on
+% surf(Z, Y, X)
+% [verts, ~] = streamslice(ZT,YT,XT, zeros(100,100,100),  zeros(100, 100,100), u(raggio), [],[],[1,2,3,4,5])
+% 
+% streamtube(verts,1)
+% lighting gouraud
+% shading interp
+
+%% 2
+
+clc
+clear
+close all
+
+
 
 
 
