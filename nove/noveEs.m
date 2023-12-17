ec8bb7aa6293f03d8a52627687f1d0dafc9d8262
Michele <13dona.michele@gmail.com>
2023-12-17 23:31:39 +0100

    nove aggiunta altri numeri calcolati

--- nove/noveEs.m
+++ nove/noveEs.m
@@ -10,9 +10,20 @@ f0 = [0,0,1]; % VA UTILIZZATO 0 1 0 PER VENIRE GIUSTO, PERCHè?
 % Ipotesi: metto 0 1 0 perché chiudo il "cerchio" di ipotesi allo stesso
 % grado di derivazione, e "apro" il cerchio nuovo dopo, così da avere
 % condizione iniziale e finale una dietro l'altra
-etaSpan = [0, 11]; % max valore 11, dopo il solver ha problemi, il warning è utile per capire se soluzione è accettabile o no
+maxEta = 6;
+etaSpan = [0, maxEta]; % max valore 11, dopo il solver ha problemi, il warning è utile per capire se soluzione è accettabile o no
 
-[eta, f, fPrimo] = bvpSolveBlasius(etaSpan, f0);
+[eta, f, fPrimo, fSecondo] = bvpSolveBlasius(etaSpan, f0);
 
 
 plot(fPrimo, eta, LineWidth=1.5)
+grid on
+
+spessoreDiSpostamento = sqrt(2) * (trapz(eta, 1-fPrimo)) % delta star
+spessoreQDM = sqrt(2) * (trapz(eta, fPrimo.*(1-fPrimo))) % teta
+coefficienteDiForma = spessoreDiSpostamento/spessoreQDM % H
+
+tauWall = fSecondo(1)/sqrt(2)
+cf = tauWall/(1/2)
+
+Df = trapz(tauWall)
