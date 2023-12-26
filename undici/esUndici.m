d74d4d97ea6f75fe18a644934cb5f57bf01adefb
Michele <13dona.michele@gmail.com>
2023-12-26 21:26:09 +0100

    esercitazione 11, fine esercizio 2

--- undici/esUndici.m
+++ undici/esUndici.m
@@ -3,15 +3,38 @@ clc
 clear
 close all
 
+lunghezza = 1.1;
+profondita = 0.75;
+viscositaCinematica = 1.5*10^-5;
+densita = 1.25;
+velocita = 5;
+
+re = velocita*lunghezza/viscositaCinematica;
+
+if re > 5*10^5
+    disp("NON TUTTO LAMINARE")
+else
+    disp("TUTTO LAMINARE")
+end
 
 
+spessoreStratoLimite = 5*lunghezza/sqrt(re)
+reX = @(x) velocita*x./viscositaCinematica;
+cf = @(x) 0.664./sqrt(reX(x));
+cf023 = cf(0.23)
+
+% drag = 1/2 * densita * u^2 * cf
+% moltiplico per 2 perché drag c'è sia sopra che sotto
+resistenzaTotale = integral(@(x) 1/2 * densita * velocita^2 * cf(x)*profondita, 0, lunghezza)*2
+
+resistenzaTotaleMetodo2 = 2*0.664*profondita*sqrt(densita^2*viscositaCinematica*lunghezza*velocita^3)
 %% 2
 clc
 clear
 close all
 
 lunghezza = 2.45;
-spessore = 2.15;
+profondita = 2.15;
 viscositaCinematica = 1.5*10^-5; %ni
 densita = 1.25;
 uInf = 3.6;
@@ -27,21 +50,32 @@ else
     disp("TUTTO LAMINARE")
 end
 
-xSpace = linspace(0, xCritico);
 tauWall = @(x) 0.332 * viscositaCinematica*densita * uInf * sqrt(uInf./(viscositaCinematica.*x));
-tauWallCritico = 0.332 * viscositaCinematica*densita * uInf * sqrt(uInf/(viscositaCinematica*xCritico))
+tauWallCritico = tauWall(xCritico)
 
-cf = integral(@(x) tauWall(x)./(1/2*densita*uInf^2)*2, 0, xCritico)
+cf = integral(@(x) tauWall(x)./(1/2*densita*uInf^2), 0, xCritico)
 
-D = cf*1/2*densita*uInf^2*2
+D = cf*1/2*densita*uInf^2*2*profondita
+DAltroMetodo = 2*0.664*profondita*sqrt(densita^2*viscositaCinematica*xCritico*uInf^3)
 
-% portata al dt = rho*u*dA  = rho * u * 1 * dy
+reX = @(x) uInf*x./viscositaCinematica;
+spessoreStratoLimite = @(x) 5 * x./sqrt(reX(x));
+spessoreMassimo = spessoreStratoLimite(xCritico);
+
+eta = @(y) y ./ spessoreMassimo;
+velocita = @(y) uInf .* ((2.*eta(y) - eta(y).^2).*(y <= spessoreMassimo));
+
+portata = integral(@(y) densita.*velocita(y), 0, spessoreMassimo)
 
 
-% reX = @(x) uInf*x/viscositaCinematica;
+
+%% es 3
+clc
+clear
+close all
 
 
-% spessoreStratoLimite = 5 * xCritico/sqrt(rexIntegro)
+% deltaStar = 1 - u/Uinf
 
 
 
