6d7a8925de2045c08cc704cddaaa7b229c7c6cef
Michele <13dona.michele@gmail.com>
2024-01-16 18:21:49 +0100

    aggiunta strato limite 7.2 e aggiunta ; a flussi paralleli

--- esLibro/FlussiParalleli.m
+++ esLibro/FlussiParalleli.m
@@ -13,11 +13,11 @@ viscositaDinamica = viscositaCinematica * densita;
 
 % portata = densita*velocita*area
 
-velocitaMax = viscositaCinematica/diametro * reCritico
+velocitaMax = viscositaCinematica/diametro * reCritico;
 portata = velocitaMax*pi*(diametro/2)^2
 
 % portata = -pi/8 * R^4/mu * dp/dx
-dp_dx = -portata * viscositaDinamica/(diametro/2)^4 * 8/pi
+dp_dx = -portata * viscositaDinamica/(diametro/2)^4 * 8/pi;
 
 pExit = 0.5*100000 + dp_dx*lunghezza
 
--- esLibro/StratoLimite.m
+++ esLibro/StratoLimite.m
@@ -18,3 +18,49 @@ eta = u/uInf;
 delta = y/eta;
 % delta = 3.5*sqrt(viscositaCinematica * x/uInf)
 x = (delta/3.5)^2*uInf/viscositaCinematica
+
+
+
+%% 7.2
+
+clc
+clear
+close all
+
+lunghezza = 4;
+profondita = 2;
+viscositaCinematica = 1.6*10^-5; %ni
+densita = 1.164;
+uInf = 2;
+
+
+reXCritico = 5*10^5;
+
+% Rex = uInf*x/viscositaCinematica
+xCritico = reXCritico * viscositaCinematica/uInf
+if xCritico < lunghezza
+    disp("NON TUTTO LAMINARE")
+else
+    disp("TUTTO LAMINARE")
+end
+
+tauWall = @(x) 0.332 * viscositaCinematica*densita * uInf * sqrt(uInf./(viscositaCinematica.*x));
+tauWallCritico = tauWall(xCritico)
+
+cf = integral(@(x) tauWall(x)./(1/2*densita*uInf^2), 0, xCritico)
+
+D = cf*1/2*densita*uInf^2*profondita
+DAltroMetodo = 0.664*profondita*sqrt(densita^2*viscositaCinematica*xCritico*uInf^3)
+
+reX = @(x) uInf*x./viscositaCinematica;
+spessoreStratoLimite = @(x) 5 * x./sqrt(reX(x));
+spessoreMassimo = spessoreStratoLimite(xCritico);
+
+eta = @(y) y ./ spessoreMassimo;
+velocita = @(y) uInf .* ((2.*eta(y) - eta(y).^2).*(y <= spessoreMassimo));
+
+portata = integral(@(y) densita.*velocita(y), 0, spessoreMassimo)*profondita
+
+
+
+
