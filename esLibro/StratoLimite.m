02cf08c08635d000da665fb7678c55467a0efb2b
Michele <13dona.michele@gmail.com>
2024-01-17 19:49:01 +0100

    Aggiunti es proposti strato limite

--- esLibro/StratoLimite.m
+++ esLibro/StratoLimite.m
@@ -21,7 +21,7 @@ x = (delta/3.5)^2*uInf/viscositaCinematica
 
 
 
-%% 7.2
+%% 7.2 -- non so da dove l'abbia preso
 
 clc
 clear
@@ -60,6 +60,56 @@ eta = @(y) y ./ spessoreMassimo;
 velocita = @(y) uInf .* ((2.*eta(y) - eta(y).^2).*(y <= spessoreMassimo));
 
 portata = integral(@(y) densita.*velocita(y), 0, spessoreMassimo)*profondita
+
+
+%% 2
+
+clc
+clear
+close all
+
+lunghezza = 0.45;
+larghezza = 0.15;
+viscositaCinematica = 0.9*10^-4;
+densita = 925;
+velocita = 6;
+
+
+re = velocita*lunghezza/viscositaCinematica
+
+if re > 5*10^5
+    disp("NON TUTTO LAMINARE")
+else
+    disp("TUTTO LAMINARE")
+end
+
+spessoreStratoLimite = 5*lunghezza/sqrt(re)
+tauWallFun = @(x) 0.332 * viscositaCinematica*densita*velocita*sqrt(velocita./(viscositaCinematica.*x))
+tauWall = tauWallFun(lunghezza)
+
+resistenzaTotale = 0.664*sqrt(viscositaCinematica*densita^2*lunghezza*velocita^3)*larghezza*2 % 2 facce
+% resistenzaAltroMetodo = trapz(linspace(0.00001, lunghezza), tauWallFun(linspace(0.00001, lunghezza)))*spessore
+
+reX = @(x) velocita*x./viscositaCinematica;
+cf = @(x) 0.664./sqrt(reX(x));
+
+resistenzaTotaleDue = integral(@(x) 1/2 * densita * velocita^2 * cf(x)*larghezza, 0, lunghezza)*2
+
+
+%% 3
+clc
+clear
+close all
+
+densita = 1000;
+viscositaDinamica = 1.307*10^-3;
+velocita = 0.5;
+
+reCritico = 5*10^5;
+% re = rho*velocita*lunghezza/mu;
+
+lunghezzaCritica = reCritico*viscositaDinamica/(velocita*densita)
+spessore = 5 * lunghezzaCritica/sqrt(reCritico)
 
 
 
