0963767030501565aadfd93c40d2c3f4f2a9e431
Michele <13dona.michele@gmail.com>
2024-01-29 20:34:08 +0100

    rimozione linspace inutile

--- esLibro/VorticiPozziEtcRifai.m
+++ esLibro/VorticiPozziEtcRifai.m
@@ -21,7 +21,6 @@ a = 0.5;
 u = 35;
 circuitazione = 200;
 
-spazio = linspace(0, 2*pi);
 syms tetaAngolo
 vTeta = @(tetaAngolo) 2*u*sin(tetaAngolo) + circuitazione/(2*pi*a);
 % cp = @(teta) 1-(vTeta(teta)./u).^2;
